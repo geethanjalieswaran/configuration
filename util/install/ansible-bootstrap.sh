211e938ea1d4246728b05110a97be147fb838608
geethanjalieswaran <geethanjali.eswaran@gmail.com>
2017-02-13 11:53:58 +0530

    Update ansible-bootstrap.sh

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -40,10 +40,10 @@ fi
 #
 # Bootstrapping constants
 #
-VIRTUAL_ENV_VERSION="15.0.2"
-PIP_VERSION="8.1.2"
-SETUPTOOLS_VERSION="24.0.3"
-VIRTUAL_ENV="/tmp/bootstrap"
+#VIRTUAL_ENV_VERSION="15.0.2"
+#PIP_VERSION="8.1.2"
+#SETUPTOOLS_VERSION="24.0.3"
+#VIRTUAL_ENV="/tmp/bootstrap"
 PYTHON_BIN="${VIRTUAL_ENV}/bin"
 ANSIBLE_DIR="/tmp/ansible"
 CONFIGURATION_DIR="/tmp/configuration"
@@ -65,44 +65,44 @@ CONFIGURATION_VERSION="${CONFIGURATION_VERSION}"
 EOF
 
 
-if [[ $(id -u) -ne 0 ]] ;then
-    echo "Please run as root";
-    exit 1;
-fi
+#if [[ $(id -u) -ne 0 ]] ;then
+#    echo "Please run as root";
+#    exit 1;
+#fi
 
-if grep -q 'Precise Pangolin' /etc/os-release
-then
-    SHORT_DIST="precise"
-elif grep -q 'Trusty Tahr' /etc/os-release
-then
-    SHORT_DIST="trusty"
-elif grep -q 'Xenial Xerus' /etc/os-release
-then
-    SHORT_DIST="xenial"
-else
-    cat << EOF
+#if grep -q 'Precise Pangolin' /etc/os-release
+#then
+#    SHORT_DIST="precise"
+#elif grep -q 'Trusty Tahr' /etc/os-release
+#then
+#    SHORT_DIST="trusty"
+#elif grep -q 'Xenial Xerus' /etc/os-release
+#then
+#    SHORT_DIST="xenial"
+#else
+#    cat << EOF
 
-    This script is only known to work on Ubuntu Precise, Trusty and Xenial,
-    exiting.  If you are interested in helping make installation possible
-    on other platforms, let us know.
+#    This script is only known to work on Ubuntu Precise, Trusty and Xenial,
+#    exiting.  If you are interested in helping make installation possible
+#    on other platforms, let us know.
 
-EOF
-   exit 1;
-fi
+#EOF
+#   exit 1;
+#fi
 
 EDX_PPA="deb http://ppa.edx.org ${SHORT_DIST} main"
 
 # Upgrade the OS
-apt-get update -y
-apt-key update -y
+#apt-get update -y
+#apt-key update -y
 
-if [ "${UPGRADE_OS}" = true ]; then
-    echo "Upgrading the OS..."
-    apt-get upgrade -y
-fi
+#if [ "${UPGRADE_OS}" = true ]; then
+#    echo "Upgrading the OS..."
+#    apt-get upgrade -y
+#fi
 
 # Required for add-apt-repository
-apt-get install -y software-properties-common python-software-properties
+#apt-get install -y software-properties-common python-software-properties
 
 # Add git PPA
 add-apt-repository -y ppa:git-core/ppa
@@ -121,7 +121,8 @@ apt-get install -y python2.7 python2.7-dev python-pip python-apt python-yaml pyt
 
 # Workaround for a 16.04 bug, need to upgrade to latest and then
 # potentially downgrade to the preferred version.
-if [[ "xenial" = "${SHORT_DIST}" ]]; then
+if [[ "xenial" = "${
+}" ]]; then
     #apt-get install -y python2.7 python2.7-dev python-pip python-apt python-yaml python-jinja2
     pip install --upgrade pip
     pip install --upgrade pip=="${PIP_VERSION}"
